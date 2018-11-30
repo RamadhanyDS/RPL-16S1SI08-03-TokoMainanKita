5631655704f9b26a1f04214533cd1f19d44f3a57
RamadhanyDS <ramadhany.d.s@gmail.com>
2018-11-30 16:45:38 +0700

    karyawan_9485

---
+++ project/data/karyawan.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tokomainankita;
+
+/**
+ *
+ * @author windows10
+ */
+public class karyawan {
+    private int id_karyawan;
+    private String nama;
+    private String alamat;
+    private String username;
+    private String password;
+    /**
+     * @return the id_karyawan
+     */
+    public int getId_karyawan() {
+        return id_karyawan;
+    }
+
+    /**
+     * @param id_karyawan the id_karyawan to set
+     */
+    public void setId_karyawan(int id_karyawan) {
+        this.id_karyawan = id_karyawan;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+   
+}
